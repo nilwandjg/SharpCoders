d996c4d0c5b4af4f696a64fffd6d6bf67f31361d
nilwandjg <nilwandjg@yahoo.com.br>
2023-12-11 21:01:39 -0300

    Implementando RxJS e Requisições

---
+++ Angular/aula4/src/app/componentes/endereco/endereco.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Endereco } from '../../modelos/endereco';
+import { EnderecoService } from '../../servicos/endereco.service';
+
+@Component({
+  selector: 'app-endereco',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './endereco.component.html',
+  styleUrl: './endereco.component.css'
+})
+export class EnderecoComponent {
+
+  //Variável CEP
+  cep: string = '';
+
+  //Construtor
+  constructor(private servico: EnderecoService) { }
+
+  //Função para obter endereço
+  obterEndereco(): void {
+    this.servico.retornarEndereco(this.cep)
+      .subscribe(retorno => { console.log(retorno) });
+  }
+
+}
